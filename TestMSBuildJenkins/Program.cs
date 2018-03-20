3a05d1ff0a08d7c128490f30f35f27e95460eb91
Semere Tekleab <tekl8585@stthomas.edu>
2018-03-20 10:54:06 -0500

    MSBuildJenkinsBuild

---
+++ TestMSBuildJenkins/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMSBuildJenkins
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			for(int i=0; i<10000;i++)
+			{
+				Console.WriteLine("------------------------" + i);
+			}
+
+		}
+	}
+}
